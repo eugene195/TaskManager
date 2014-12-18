489e134c9d223303c589edcda83e4a372d9d4bcd
nfetissow <nfetissow@gmail.com>
2014-12-18 20:50:53 +0300

    Added Multiplier task

---
+++ src/main/java/Multiplier.java
@@ -0,0 +1,9 @@
+/**
+ * Created by Nikita on 18.12.2014.
+ */
+public class Multiplier extends Task {
+    public Multiplier() {
+        this.type = "MLT";
+    }
+
+}
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -2,7 +2,7 @@
  * Created by eugene on 12/18/14.
  */
 public abstract class Task {
-    private String type;
+    protected String type;
 
     public String getType() {
         return type;
